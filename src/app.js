b9148ced7092a58219350e1b84f1faf265a0235a
Vaftir <yagoassisfaria@gmail.com>
2022-06-08 23:05:11 -0300

    FUNCIONA API CARALHO

---
+++ src/app.js
@@ -0,0 +1,79 @@
+//import {openDb} from './configDB.js';
+import express from 'express';
+
+import { createTable, insertPessoa } from './Controller/Pessoa.js';
+
+
+
+const app = express();
+
+app.use(express.json());
+
+
+createTable();
+insertPessoa();
+
+/*
+
+var db;
+var consumo;
+var tempo;
+var today = new Date();
+        var endTime = new Date();
+        var startTime = new Date();
+
+        var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        var time = (today.getHours()) + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date + ' ' + time;
+        endTime = dateTime;
+
+        var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        var time = (today.getHours() -1) + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date + ' ' + time;
+        startTime = dateTime;
+
+
+
+
+
+        console.log(startTime)
+        console.log(endTime)
+
+    
+        var key = "6615871e-6222-4e2e-9325-f02e611ff39f";
+        var url = "https://api.tago.io/data?variable=valor1&start_date=" + startTime.toString() + "&end_date=" + endTime.toString();
+
+
+function inserT(tempo, consumo){
+    openDb().then(db=>{
+        db.run('INSERT INTO IOT (tempo, gasto) VALUES (?,?)', tempo, consumo);
+    });
+}
+
+fetch(url, {
+     method: 'GET',
+        
+    headers: { 
+         'Authorization': '6615871e-6222-4e2e-9325-f02e611ff39f'
+    }
+ }).then(res => res.json())
+   .then(json => {
+        db = json
+        for(var i = 0; i < db["result"].length; i++){
+            tempo = db["result"][i].time
+            consumo = db["result"][i].value
+            inserT(tempo,consumo)
+        }
+        
+    })
+   .catch(err => console.log(err));
+
+
+*/
+app.listen(3000, () => {
+    console.log("api on");
+})
+
+
+
+
